5932ed146e619aa47d2441a05f486527cad665dc
SatoshiiST <100876955+SatoshiiST@users.noreply.github.com>
2023-12-02 16:54:16 -0300

    Teste de subir os arquivos do site

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function imc ()
+{
+    var Nome = document.querySelector("#inNome");
+    var Alturatag = document.querySelector("#inAltura");
+    var Pesotag = document.querySelector("#inPeso")
+
+    var Altura = Alturatag.value / 100;
+    var Peso = Pesotag.value;
+
+    var imc = Peso / Altura **2;
+    if(imc < 18.5)
+    {
+        alert('Olá ' + Nome.value + ', você está magro');
+    }
+    else if(imc < 24.9)
+    {
+        alert('Olá ' + Nome.value + ', você está normal');
+    }
+    else if(imc < 29.9)
+    {
+        alert('Olá ' + Nome.value + ', você está normal');
+    }
+    else if(imc < 39.9)
+    {
+        alert('Olá ' + Nome.value + ', você está normal');
+    }
+    else if(imc> 39.9)
+    {
+        alert('Olá ' + Nome.value + ', você está normal');
+    }
+}
+
