fad7518451c4a43c7dc67ca982bf53b9ffc2d32a
SatoshiiST <100876955+SatoshiiST@users.noreply.github.com>
2023-12-06 10:52:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function imc ()
 {
     var Nome = document.querySelector("#inNome");
     var Alturatag = document.querySelector("#inAltura");
-    var Pesotag = document.querySelector("#inPeso")
+    var Pesotag = document.querySelector("#inPeso");
 
     var Altura = Alturatag.value / 100;
     var Peso = Pesotag.value;
@@ -19,15 +19,15 @@ function imc ()
     }
     else if(imc < 29.9)
     {
-        alert('Olá ' + Nome.value + ', você está normal');
+        alert('Olá ' + Nome.value + ',você está acima do peso');
     }
     else if(imc < 39.9)
     {
-        alert('Olá ' + Nome.value + ', você está normal');
+        alert('Olá ' + Nome.value + ', você está obeso');
     }
     else if(imc> 39.9)
     {
-        alert('Olá ' + Nome.value + ', você está normal');
+        alert('Olá ' + Nome.value + ', você está em obesidade morbida');
     }
 }
 
